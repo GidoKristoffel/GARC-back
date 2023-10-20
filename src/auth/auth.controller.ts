2a31693892b7a8307eab78d3d51b74f239a5836b
DmitryR <dmytro.ryabchun@gmail.com>
2023-10-20 23:28:41 +0300

    🚧 Improved logic of registration and login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,38 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { LoginDto, RegisterDto } from '@auth/dto';
+import { AuthService } from '@auth/auth.service';
+import { Tokens } from "@auth/interfaces";
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('registration')
-  registration(@Body() dto: RegisterDto) {}
+  async registration(@Body() dto: RegisterDto) {
+    const user = await this.authService.register(dto);
+    if (!user) {
+      throw new BadRequestException(
+        `Can't register user with data ${JSON.stringify(dto)}`,
+      );
+    }
+  }
 
   @Post('login')
-  login(@Body() dto: LoginDto) {}
+  async login(@Body() dto: LoginDto): Promise<{ accessToken: string }> {
+    const tokens: Tokens = await this.authService.login(dto);
+    if (!tokens) {
+      throw new BadRequestException(
+        `Can't login with data ${JSON.stringify(dto)}`,
+      );
+    }
+    return { accessToken: tokens.accessToken };
+  }
 
   @Get('refresh')
   refreshTokens() {}
