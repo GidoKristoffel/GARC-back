0ec96bf9e5bbd25552da93f2c493a8c07d2681fb
DmitryR <dmytro.ryabchun@gmail.com>
2023-10-24 23:18:25 +0300

    🚧 Improved user deletion logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,36 +1,36 @@
 import {
-  Body, ClassSerializerInterceptor,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
   Param,
   ParseUUIDPipe,
-  Post, UseInterceptors
-} from "@nestjs/common";
+  UseInterceptors
+} from '@nestjs/common';
 import { UserService } from '@user/user.service';
-import { UserResponse } from "@user/responses";
+import { UserResponse } from '@user/responses';
+import { CurrentUser } from '@common/decorators';
+import { JwtPayload } from '@auth/interfaces';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UseInterceptors(ClassSerializerInterceptor)
-  @Post()
-  async createUser(@Body() dto): Promise<UserResponse> {
-    const user = await this.userService.save(dto);
-    return new UserResponse(user);
-  }
-
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':idOrEmail')
-  async findOneUser(@Param('idOrEmail') idOrEmail: string): Promise<UserResponse> {
+  async findOneUser(
+    @Param('idOrEmail') idOrEmail: string,
+  ): Promise<UserResponse> {
     const user = await this.userService.findOne(idOrEmail);
     return new UserResponse(user);
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
   @Delete(':id')
-  async deleteUser(@Param('id', ParseUUIDPipe) id: string) {
-    return await this.userService.delete(id);
+  async deleteUser(
+    @Param('id', ParseUUIDPipe) id: string,
+    @CurrentUser() user: JwtPayload,
+  ): Promise<{ id: string }> {
+    return await this.userService.delete(id, user);
   }
 }
