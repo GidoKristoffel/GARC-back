7f8a9f11ab5321c6b7fab3b7413453712987a380
DmitryR <dmytro.ryabchun@gmail.com>
2023-10-27 23:08:09 +0300

    ✨ Added token caching

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,17 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
 import { Role, User } from '@prisma/client';
 import { genSaltSync, hashSync } from 'bcrypt';
 import { JwtPayload } from '@auth/interfaces';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   save(user: Partial<User>) {
     const hashedPassword: string = this.hashPassword(user.password);
@@ -19,19 +24,28 @@ export class UserService {
     });
   }
 
-  findOne(idOrEmail: string) {
-    return this.prismaService.user.findFirst({
-      where: {
-        OR: [
-          {
-            id: idOrEmail,
-          },
-          {
-            email: idOrEmail,
-          },
-        ],
-      },
-    });
+  async findOne(idOrEmail: string) {
+    const user = await this.cacheManager.get<User>(idOrEmail);
+    if (!user) {
+      const user = await this.prismaService.user.findFirst({
+        where: {
+          OR: [
+            {
+              id: idOrEmail,
+            },
+            {
+              email: idOrEmail,
+            },
+          ],
+        },
+      });
+      if (!user) {
+        return null;
+      }
+      await this.cacheManager.set(idOrEmail, user);
+      return user;
+    }
+    return user;
   }
 
   delete(id: string, user: JwtPayload) {
