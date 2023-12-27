a1b3fd28f254637bb940595e13c8268619e356fb
DmitryR <dmytro.ryabchun@gmail.com>
2023-12-27 21:19:23 +0200

    ✨ Added level up material scheme

--- src/modules/ascension-material/ascension-material.module.ts
+++ src/modules/ascension-material/ascension-material.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AscensionMaterialService } from './services/ascension-material/ascension-material.service';
 import { TransformAscensionMaterialService } from './services/transform-ascension-material/transform-ascension-material.service';
-import { AscensionMaterialService } from './controllers/ascension-material/ascension-material.service';
 import { AscensionMaterialController } from './controllers/ascension-material/ascension-material.controller';
+import { AscensionMaterialService } from './services/ascension-material/ascension-material.service';
 
 @Module({
   providers: [AscensionMaterialService, TransformAscensionMaterialService],
-  controllers: [AscensionMaterialController]
+  controllers: [AscensionMaterialController],
 })
 export class AscensionMaterialModule {}
