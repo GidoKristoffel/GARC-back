d466d5f1c3d7537c7317fffeb1e22b43af170ca1
DmitryR <dmytro.ryabchun@gmail.com>
2024-01-01 12:36:54 +0200

    🚧 Improved weapon material module

---
+++ src/modules/weapon-material/services/transform-weapon-material/transform-weapon-material.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { WeaponMaterial } from '@prisma/client';
+import {
+  IWeaponMaterial,
+  IWeaponMaterialCreate,
+} from '../../interfaces/common.interface';
+import { EWeaponMaterialType } from '../../../../core/enums/weapon-material-type.enum';
+import { EMaterialRarity } from '../../../../core/enums/material-rarity.enum';
+import { ERegion } from '../../../character/enums/region.enum';
+import { EDay } from '../../../../core/enums/day.enum';
+import { WeaponMaterialDto } from '../../dto';
+import { $Enums } from '.prisma/client';
+
+@Injectable()
+export class TransformWeaponMaterialService {
+  public transformToResponseFormat(
+    data: WeaponMaterial,
+  ): IWeaponMaterial | null {
+    if (!data) {
+      return null;
+    }
+    return {
+      id: data.id,
+      name: {
+        en: data.nameEn,
+        ua: data.nameUa,
+        ru: data.nameRu,
+      },
+      description: {
+        en: data.descriptionEn,
+        ua: data.descriptionUa,
+        ru: data.descriptionRu,
+      },
+      type: this.convertToKebabCase(data.type) as EWeaponMaterialType,
+      rarity: this.convertToKebabCase(data.rarity) as EMaterialRarity,
+      region: this.convertToKebabCase(data.region) as ERegion,
+      farmDays: data.farmDays.map((day) => day.toLowerCase()) as EDay[],
+      icon: data.icon,
+    };
+  }
+
+  public transformToDBFormat(
+    weaponMaterial: WeaponMaterialDto,
+  ): IWeaponMaterialCreate {
+    return {
+      nameEn: weaponMaterial.name.en,
+      nameUa: weaponMaterial.name.ua,
+      nameRu: weaponMaterial.name.ru,
+      descriptionEn: weaponMaterial.description.en,
+      descriptionUa: weaponMaterial.description.ua,
+      descriptionRu: weaponMaterial.description.ru,
+      type: weaponMaterial.type.toUpperCase() as $Enums.WeaponMaterialType,
+      rarity: weaponMaterial.rarity.toUpperCase() as $Enums.MaterialRarity,
+      region: weaponMaterial.region.toUpperCase() as $Enums.Region,
+      farmDays: weaponMaterial.farmDays.map((day: EDay) =>
+        day.toUpperCase(),
+      ) as $Enums.Day[],
+      icon: weaponMaterial.icon,
+    };
+  }
+
+  private convertToKebabCase(input: string): string {
+    return input.toLowerCase().replace(/_/g, '-');
+  }
+}
--- src/modules/weapon-material/weapon-material.module.ts
+++ src/modules/weapon-material/weapon-material.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { WeaponMaterialService } from './services/weapon-material/weapon-material.service';
+import { TransformWeaponMaterialService } from './services/transform-weapon-material/transform-weapon-material.service';
+import { WeaponMaterialController } from './controllers/weapon-material/weapon-material.controller';
 
-@Module({})
+@Module({
+  providers: [WeaponMaterialService, TransformWeaponMaterialService],
+  controllers: [WeaponMaterialController]
+})
 export class WeaponMaterialModule {}
