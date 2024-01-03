9371dcb87c4281e514691b105ff5b752b63321ce
DmitryR <dmytro.ryabchun@gmail.com>
2024-01-03 22:14:58 +0200

    🚧 Improved enhancement material module

---
+++ src/modules/enhancement-material/interfaces/response.interface.ts
@@ -0,0 +1,27 @@
+import { HttpStatus } from "@nestjs/common";
+import { IDeletedEnhancementMaterial, IEnhancementMaterial } from "./common.interface";
+
+export interface IAllEnhancementMaterialsResponse {
+	enhancementMaterials: IEnhancementMaterial[];
+	status: HttpStatus.FOUND | HttpStatus.NOT_FOUND;
+}
+
+export interface IEnhancementMaterialResponse {
+	enhancementMaterial: IEnhancementMaterial;
+	status: HttpStatus.FOUND | HttpStatus.NOT_FOUND;
+}
+
+export interface ICreatedEnhancementMaterialResponse {
+	enhancementMaterial: IEnhancementMaterial;
+	status: HttpStatus.CREATED | HttpStatus.BAD_REQUEST;
+}
+
+export interface IUpdatedEnhancementMaterialResponse {
+	enhancementMaterial: IEnhancementMaterial;
+	status: HttpStatus.OK | HttpStatus.BAD_REQUEST;
+}
+
+export interface IDeletedEnhancementMaterialResponse {
+	enhancementMaterial: IDeletedEnhancementMaterial | null;
+	status: HttpStatus.OK | HttpStatus.BAD_REQUEST;
+}
