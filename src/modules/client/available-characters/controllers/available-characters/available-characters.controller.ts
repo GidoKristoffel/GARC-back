18d84d5d252d279c2cf93024a461a2056c4c4234
DmitryR <dmytro.ryabchun@gmail.com>
2024-02-07 21:16:22 +0200

    ✨ Added controller and common interface to available characters module

---
+++ src/modules/client/available-characters/controllers/available-characters/available-characters.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('available-characters')
+export class AvailableCharactersController {}
---
+++ src/modules/client/available-characters/interfaces/common.interface.ts
@@ -0,0 +1,13 @@
+export interface IAvailableCharacter {
+  userId: string;
+  characterId: string;
+}
+
+export interface IAvailableCharacterCreate {
+  userId: string;
+  characterId: string;
+}
+
+export interface IDeletedAvailableCharacter {
+  id: string;
+}
