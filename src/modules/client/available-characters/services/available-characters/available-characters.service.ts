7a8ef86a21668070f29f7df1054cf6df53fb520e
DmitryR <dmytro.ryabchun@gmail.com>
2024-02-08 23:13:38 +0200

    🚧 Updated service for available characters module

--- src/modules/client/available-characters/services/available-characters/available-characters.service.ts
+++ src/modules/client/available-characters/services/available-characters/available-characters.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../../shared/prisma/prisma.service';
+import { AscensionMaterial, Prisma, PrismaPromise } from '@prisma/client';
+import {
+  IAvailableCharacter,
+  IAvailableCharacterCreate,
+} from '../../interfaces/common.interface';
 
 @Injectable()
-export class AvailableCharactersService {}
+export class AvailableCharactersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public async getByUserId(
+    userId: string,
+  ): Promise<IAvailableCharacter[] | null> {
+    return this.prismaService.userCharacters
+      .findMany({
+        where: {
+          userId,
+        },
+      })
+      .catch(() => null);
+  }
+
+  public add(
+    userId: string,
+    characterIds: string[],
+  ): Promise<Prisma.BatchPayload> {
+    return this.prismaService.userCharacters.createMany({
+      data: characterIds.map((characterId: string): IAvailableCharacter => {
+        return {
+          userId,
+          characterId,
+        };
+      }),
+      skipDuplicates: true,
+    });
+  }
+
+  public async remove(
+    userId: string,
+    characterIds: string[],
+  ): Promise<Prisma.BatchPayload> {
+    return this.prismaService.userCharacters.deleteMany({
+      where: {
+        userId: userId,
+        characterId: { in: characterIds },
+      },
+    });
+  }
+}
