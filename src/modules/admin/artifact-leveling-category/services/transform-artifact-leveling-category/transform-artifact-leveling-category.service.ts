df9d69e472cfd409bd80d3ab213e49bf5fd17eb6
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-16 20:41:35 +0300

    🚧 Updated transform service for artifact leveling category module

--- src/modules/admin/artifact-leveling-category/services/transform-artifact-leveling-category/transform-artifact-leveling-category.service.ts
+++ src/modules/admin/artifact-leveling-category/services/transform-artifact-leveling-category/transform-artifact-leveling-category.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ArtifactLevelingCategory } from '@prisma/client';
+import { IArtifactLevelingCategory } from '../../interfaces/common.interface';
 
 @Injectable()
-export class TransformArtifactLevelingCategoryService {}
+export class TransformArtifactLevelingCategoryService {
+  public transformToResponseFormat(
+    data: ArtifactLevelingCategory,
+  ): IArtifactLevelingCategory | null {
+    if (!data) {
+      return null;
+    }
+    return {
+      id: data.id,
+      name: {
+        en: data.nameEn,
+        ua: data.nameUa,
+        ru: data.nameRu,
+      },
+      index: data.index,
+    };
+  }
+
+  private convertToKebabCase(input: string): string {
+    return input.toLowerCase().replace(/_/g, '-');
+  }
+}
