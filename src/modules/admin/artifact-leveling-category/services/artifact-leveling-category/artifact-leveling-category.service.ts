7b648e74de664b04ea5525816478fee8c2cf3ac5
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-19 20:09:27 +0300

    🚧 Updated service for artifact leveling category module

--- src/modules/admin/artifact-leveling-category/services/artifact-leveling-category/artifact-leveling-category.service.ts
+++ src/modules/admin/artifact-leveling-category/services/artifact-leveling-category/artifact-leveling-category.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { IAscensionMaterial } from '../../../ascension-material/interfaces/common.interface';
-import { ArtifactLevelingCategory, AscensionMaterial } from '@prisma/client';
+import { ArtifactLevelingCategory, AscensionMaterial } from "@prisma/client";
 import { PrismaService } from '../../../../shared/prisma/prisma.service';
 import { TransformArtifactLevelingCategoryService } from '../transform-artifact-leveling-category/transform-artifact-leveling-category.service';
 import { IArtifactLevelingCategory } from '../../interfaces/common.interface';
+import { AscensionMaterialDto } from "../../../ascension-material/dto";
+import { ArtifactLevelingCategoryDto } from "../../dto";
 
 @Injectable()
 export class ArtifactLevelingCategoryService {
@@ -27,7 +29,7 @@ export class ArtifactLevelingCategoryService {
       .catch(() => null);
   }
 
-  public async findAll(): Promise<IAscensionMaterial[] | null> {
+  public async findAll(): Promise<IArtifactLevelingCategory[] | null> {
     return this.prismaService.artifactLevelingCategory
       .findMany()
       .then((artifactLevelingCategories: ArtifactLevelingCategory[]) => {
@@ -39,5 +41,19 @@ export class ArtifactLevelingCategoryService {
         );
       })
       .catch(() => null);
+  }
+
+  public async create(
+    artifactLevelingCategory: ArtifactLevelingCategoryDto,
+  ): Promise<IArtifactLevelingCategory | null> {
+    const createdArtifactLevelingCategory: ArtifactLevelingCategory =
+      await this.prismaService.artifactLevelingCategory.create({
+        data: this.transformArtifactLevelingCategoryService.transformToDBFormat(
+          artifactLevelingCategory,
+        ),
+      });
+    return this.transformArtifactLevelingCategoryService.transformToResponseFormat(
+      createdArtifactLevelingCategory,
+    );
   }
 }
