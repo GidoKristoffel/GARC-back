8bc3a0fadce6809b60d1032d7cd4d9f9e1947d2f
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-22 20:45:14 +0300

    🚧 Updated controller for artifact leveling category module

--- src/modules/admin/artifact-leveling-category/controllers/artifact-leveling-category/artifact-leveling-category.controller.ts
+++ src/modules/admin/artifact-leveling-category/controllers/artifact-leveling-category/artifact-leveling-category.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpStatus,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ArtifactLevelingCategoryService } from '../../services/artifact-leveling-category/artifact-leveling-category.service';
+import { IAllArtifactLevelingCategoryResponse } from '../../interfaces/response.interface';
+import { IArtifactLevelingCategory } from '../../interfaces/common.interface';
 
 @Controller('artifact-leveling-category')
-export class ArtifactLevelingCategoryController {}
+export class ArtifactLevelingCategoryController {
+  constructor(
+    private artifactLevelingCategoryService: ArtifactLevelingCategoryService,
+  ) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get()
+  async getAllArtifactLevelingCategory(): Promise<IAllArtifactLevelingCategoryResponse> {
+    const artifactLevelingCategories: IArtifactLevelingCategory[] | null =
+      await this.artifactLevelingCategoryService.findAll();
+    return {
+      artifactLevelingCategories,
+      status: artifactLevelingCategories
+        ? HttpStatus.FOUND
+        : HttpStatus.NOT_FOUND,
+    };
+  }
+}
